0c147fdb0dd20741095b3d08613e5204cc8ff41c
Nguyễn Đình Quân <mrnarga@gmail.com>
2012-03-18 23:43:31 +0700

    translation akismet and language describtion

---
+++ vietnamese/akismet.lang.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * MyBB 1.6 Vietnamese Language Pack
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ * 
+ * $Id: akismet.lang.php 5557 2011-08-26 22:37:22Z narga $
+ */
+
+$l['spam'] = "Spam";
+
+$l['post_spam_success'] = "Bài viết đã được đánh dấu thành bài viết rác";
+$l['thread_spam_success'] = "Chủ đề đã được đánh dấu thành chủ đề rác";
+
+$l['mark_as_spam'] = "Đánh dấu nội dung rác";
+$l['confirm_mark_as_spam'] = "Bạn có muốn đánh dấu bài viết/chủ đề đã được chọn là nội dung rác?";
+
+$l['akismet_error'] = "Akismet bị lỗi";
+
+$l['redirect_newthread'] = "Akismet phát hiện chủ đề này có nội dung không phù hợp và nó sẽ được giữ lại để xử lý.<br />Bạn cần liên hệ với Ban quản trị nếu có khiếu nại về vấn đề này.";
+$l['redirect_return_forum'] = "<a href=\"{1}\">Quay lại diễn đàn</a>.";
+
+$l['redirect_newreply'] = "Akismet phát hiện bài viết này có nội dung không phù hợp và nó sẽ được giữ lại để xử lý.<br />Bạn cần liên hệ với Ban quản trị nếu có khiếu nại về vấn đề này.";
+$l['redirect_return_thread']= "Bạn có thể quay lại <a href=\"{1}\">chủ đề đang xem</a>.<br />Hoặc bạn có thể quay lại <a href=\"{2}\">diễn đàn.</a>";
+
+?>
