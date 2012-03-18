f6754406109030a532f5ac24b6703bb9533fa41a
Nguyễn Đình Quân <mrnarga@gmail.com>
2012-03-19 00:07:23 +0700

    Update translated time

---
+++ vietnamese/announcements.lang.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * MyBB 1.6 English Language Pack
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ * 
+ * $Id: announcements.lang.php 5297 2010-12-28 22:01:14Z Tomm $
+ */
+
+$l['nav_announcements'] = "Thông báo của Diễn đàn";
+$l['announcements'] = "Thông báo";
+$l['forum_announcement'] = "Thông báo của Diễn đàn: {1}";
+$l['error_invalidannouncement'] = "The announcement specified is invalid.";
+
+$l['announcement_edit'] = "Thay đổi nội dung thông báo này";
+$l['announcement_qdelete'] = "Xóa thông báo này";
+$l['announcement_quickdelete_confirm'] = "Bạn có chắc chắn là muốn xóa thông báo này?";
+
+?>
