0babd5b46de06742dda7a8daa799a301fa17db23
Nguyễn Đình Quân <mrnarga@gmail.com>
2012-03-19 11:14:10 +0700

    Bổ xung các thành phần ngôn ngữ khác

---
+++ vietnamese/customhelpdocs.lang.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * MyBB 1.6 Vietnamese Language Pack
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ * 
+ * $Id: customhelpdocs.lang.php Mon 19 Mar 2012 11:12:13 AM ICT Nguyễn Đình Quân http://www.narga.org/
+ * https://github.com/Narga/MyBB-Vietnamese-Language-Pack/
+ * 
+
+/*
+ * Custom Help Document Translation Format
+ *
+ * // Help Document {hid}
+ * $l['d{hid}_name'] = "Document name";
+ * $l['d{hid}_desc'] = "Document description";
+ * $l['d{hid}_document'] = "Document text";
+ */
+?>
---
+++ vietnamese/customhelpsections.lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * MyBB 1.6 Vietnamese Language Pack
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ * 
+ * $Id: customhelpdocs.lang.php Mon 19 Mar 2012 11:12:13 AM ICT Nguyễn Đình Quân http://www.narga.org/
+ * https://github.com/Narga/MyBB-Vietnamese-Language-Pack/
+ */
+
+/*
+ * Custom Help Section Translation Format
+ *
+ * // Help Section {sid}
+ * $l['s{sid}_name'] = "Section name";
+ * $l['s{sid}_desc'] = "Section description";
+ */
+?>
