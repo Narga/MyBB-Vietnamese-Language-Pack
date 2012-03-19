6d8cdc0bfdf385740f207faf7fa11e4d9c9c95a5
Nguyễn Đình Quân <mrnarga@gmail.com>
2012-03-19 11:11:00 +0700

    Dịch xong phần lịch

---
+++ vietnamese/calendar.lang.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * MyBB 1.6 Vietnamese Language Pack
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ *
+ * $Id: calendar.lang.php Mon 19 Mar 2012 11:08:58 AM ICT Nguyễn Đình Quân http://www.narga.org/
+ * https://github.com/Narga/MyBB-Vietnamese-Language-Pack/
+ */
+
+$l['nav_calendar'] = "Lịch";
+$l['nav_viewevent'] = "Xem sự kiện";
+$l['nav_dayview'] = "Ngày xem";
+$l['nav_addevent'] = "Thêm sự kiện";
+$l['nav_editevent'] = "Sửa sự kiện";
+$l['nav_move_event'] = "Chuyển sự kiện";
+
+$l['calendar_disabled'] = "Bạn không thể sử dụng lịch vì nó đã bị khoá bởi Ban quản trị diễn đàn.";
+
+// In some languages these months need to be slightly different than those defined in global.lang.php
+$l['alt_month_1'] = "Tháng một";
+$l['alt_month_2'] = "Tháng hai";
+$l['alt_month_3'] = "Tháng ba";
+$l['alt_month_4'] = "Tháng tư";
+$l['alt_month_5'] = "Tháng năm";
+$l['alt_month_6'] = "Tháng sáu";
+$l['alt_month_7'] = "Tháng bảy";
+$l['alt_month_8'] = "Tháng tám";
+$l['alt_month_9'] = "Tháng chín";
+$l['alt_month_10'] = "Tháng mười";
+$l['alt_month_11'] = "Tháng mười một";
+$l['alt_month_12'] = "Tháng mười hai";
+$l['add_private_event'] = "Thêm sự kiện cá nhân";
+$l['add_public_event'] = "Thêm sự kiện chung";
+$l['calendar'] = "Lịch";
+$l['jump_month'] = "Chuyển đến tháng:";
+$l['birthdays_on_day'] = "Sinh nhật vào {1}";
+$l['birthdayhidden'] = "Ẩn";
+$l['event'] = "sự kiện";
+$l['events'] = "sự kiện";
+$l['add_event'] = "Thêm sự kiện";
+$l['event_date'] = "Ngày diễn ra:";
+$l['event_recurring_start_date'] = "Ngày bắt đầu:";
+$l['event_recurring_end_date'] = "Ngày kết thúc:";
+$l['event_recurring_repeat_days'] = "Sự kiện lặp lại vào:";
+$l['event_name'] = "Tên sự kiện:";
+$l['event_details'] = "Thông tin sự kiện:";
+$l['event_options'] = "Điều chỉnh:";
+$l['private_option'] = "<b>Cá nhân</b> Chỉ có bạn mới có thể xem sự kiện. (Chỉ dành cho thành viên).";
+$l['delete_option'] = "<b>Xoá:</b> Xoá sự kiện.";
+$l['post_event'] = "Post Event";
+$l['day_view'] = "Ngày xem";
+$l['birthday'] = "Sinh nhật";
+$l['birthdays'] = "Sinh nhật";
+$l['event_author'] = "Người viết sự kiện:";
+$l['edit_event'] = "Sửa sự kiện";
+$l['view_event'] = "Xem sự kiện";
+$l['no_events'] = "Đây là ngày không có bất kỳ sự kiện nào liên kết với nó.<p><a href=\"calendar.php?action=addevent&amp;calendar={1}&amp;day={2}&amp;month={3}&amp;year={4}\">Viết sự kiện</a>.</p>";
+$l['years_old'] = "{1} tuổi";
+$l['alt_edit'] = "Sửa sự kiện này";
+$l['alt_delete'] = "Xoá sự kiện này";
+$l['moderator_options'] = "Moderator Options";
+$l['approve_event'] = "Chấp nhận sự kiện";
+$l['unapprove_event'] = "Không chấp nhận sự kiện";
+$l['move_event'] = "Chuyển sự kiện";
+$l['repeats_every_day'] = "Lặp lại hàng ngày";
+$l['repeats_every_x_days'] = "Lặp lại sau từng đợt {1} ngày";
+$l['repeats_on_weekdays'] = "Lặp lại từ Thứ hai đến Thứ sáu";
+$l['every_week_on_days'] = "Lặp lại hàng tuần vào {1}";
+$l['every_week'] = "Lặp lại hàng tuần";
+$l['every_x_weeks_on_days'] = "Lặp lại vào mỗi {1} của tuần<br />{2}";
+$l['every_x_weeks'] = "Lặp lại vào mỗi {1} tuần";
+$l['every_month_on_day'] = "Lặp lại vào ngày {1}<br />của mỗi tháng";
+$l['every_x_months_on_day'] = "Lặp lại vào ngày {1}<br />của mỗi {2} tháng";
+$l['every_month_on_weekday'] = "Lặp lại vào {1} {2}<br />của mỗi tháng";
+$l['every_x_months_on_weekday'] = "Lặp lại vào {1} {2}<br />của mỗi {3} tháng";
+$l['weekday_occurance_1'] = "thứ nhất";
+$l['weekday_occurance_2'] = "thứ hai";
+$l['weekday_occurance_3'] = "thứ ba";
+$l['weekday_occurance_4'] = "thứ tư";
+$l['weekday_occurance_last'] = "cuối cùng";
+$l['every_year_on_day'] = "Lặp lại mỗi năm vào {1} {2}";
+$l['every_x_years_on_day'] = "Lặp lại mỗi {3} năm vào {1} {2}";
+$l['every_year_on_weekday'] = "Lặp lại vào {1} {2} trong {3} của mỗi năm";
+$l['every_x_year_on_weekday'] = "Lặp lại vào {1} {2} trong {3} của {4} năm";
+$l['delete_event'] = "Xoá sự kiện";
+$l['delete_q'] = "Xoá?";
+$l['delete_1'] = "Để xoá sự kiện này bạn phải check vào ô checkbox ở bên trái và click vào nút bên phải.";
+$l['delete_2'] = "<b>Chú ý:</b> Tiến trình này sẽ không thể phục hồi.";
+$l['delete_now'] = "Xoá bây giờ";
+$l['jump_to_calendar'] = "Chuyển tới";
+$l['select_calendar'] = "Lịch:";
+$l['type_single'] = "Ngày có sự kiện";
+$l['type_ranged'] = "Thứ tự hoặc chu kỳ của sự kiện";
+$l['enter_time'] = "Thời gian:";
+$l['start_time'] = "Bắt đầu:";
+$l['end_time'] = "Kết thúc:";
+$l['timezone'] = "Giờ hệ thống:";
+$l['ignore_timezone'] = "<strong>Không sử dụng giờ hệ thống:</strong> Sự kiện sẽ sử dụng giờ hệ thống của người xem.";
+$l['repeats'] = "Lặp lại:";
+$l['does_not_repeat'] = "Không được lặp lại";
+$l['repeats_daily'] = "Hàng ngày";
+$l['repeats_weekdays'] = "Hàng tuần (Từ thứ hai đến thứ sáu)";
+$l['repeats_weekly'] = "Hàng tuần";
+$l['repeats_every'] = "Lặp lại trong:";
+$l['day_or_days'] = "ngày";
+$l['week_or_weeks_on'] = "tuần";
+$l['repeats_monthly'] = "Hàng tháng";
+$l['repeats_yearly'] = "Hàng năm";
+$l['repeats_on_day'] = "Lặp lại vào ngày";
+$l['of_every'] = "của mỗi";
+$l['month_or_months'] = "tháng";
+$l['repeats_on_the'] = "Lặp lại vào lúc";
+$l['day_of_every'] = "ngày";
+$l['repeats_on'] = "Lặp lại";
+$l['every'] = "mỗi";
+$l['year_or_years'] = "năm";
+$l['of'] = "của";
+$l['move_to_calendar'] = "Chuyển đến:";
+$l['weekly_overview'] = "Xem mỗi tuần:";
+$l['previous_week'] = "Tuần trước";
+$l['next_week'] = "Tuần tiếp theo";
+$l['first'] = "thứ nhất";
+$l['second'] = "thứ nhì";
+$l['third'] = "thứ ba";
+$l['fourth'] = "thứ tư";
+$l['last'] = "cuối cùng";
+$l['all_day'] = "Toàn bộ ngày";
+$l['starts'] = "Bắt đầu: ";
+$l['finishes'] = "Kết thúc: ";
+$l['error_incorrectday'] = "Ngày bạn chọn không tồn tại. Quay trở lại và thử lại.";
+$l['error_invalidevent'] = "Sự kiện bạn chọn không tồn tại.";
+$l['invalid_calendar'] = "Lịch bạn chọn không tồn tại. Bạn có chắc là đã chọn đúng trang?";
+$l['redirect_eventdeleted'] = "Sự kiện đã được xoá.<br />Bạn sẽ được chuyển về trang lịch.";
+$l['redirect_eventupdated'] = "Sự kiện đã được cập nhật thành công.<br />Bạn đang được chuyển về sự kiện.";
+$l['redirect_eventadded'] = "Sự kiện đã được thêm vào.<br />Bạn đang được chuyển về sự kiện.";
+$l['redirect_eventadded_moderation'] = "Sự kiện đã được thêm vào nhưng cần đợi xét duyệt trước khi công bố.<br />Bạn đang được chuyển về trang lịch.";
+$l['redirect_eventunapproved'] = "Sự kiện không được chấp thuận.<br />Bạn đang được chuyển về sự kiện.";
+$l['redirect_eventapproved'] = "Sự kiện đã được chấp thuận.<br />Bạn đang được chuyển về sự kiện.";
+?>
