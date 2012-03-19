6253a8c352691fb6f3e24c9e909496bd3e320a56
Nguyễn Đình Quân <mrnarga@gmail.com>
2012-03-19 11:21:33 +0700

    Dịch phần thông báo của tin nhăn

---
+++ vietnamese/datahandler_pm.lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * MyBB 1.6 English Language Pack
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ *
+ * $Id: datahandler_pm.lang.php Mon 19 Mar 2012 11:20:57 AM ICT Nguyễn Đình Quân http://www.narga.org/
+ * https://github.com/Narga/MyBB-Vietnamese-Language-Pack/
+ */
+
+$l['pmdata_too_long_subject'] = "Tiêu đề của tin nhắn quá dài. Bạn hãy nhập tiêu đề ngắn hơn.";
+$l['pmdata_code_no_subject'] = "[Không Tiêu Đề]";
+$l['pmdata_missing_subject'] = "Bạn chưa nhập tiêu đề cho tin nhắn. Bạn hãy nhập tiêu đề.";
+$l['pmdata_missing_message'] = "Bạn chưa nhập nội dung tin nhắn. Bạn hãy nhập nội dung.";
+$l['pmdata_invalid_recipients'] = "Có một hoặc nhiều tên người nhập không tồn tại. Tên người nhận không tồn tại: {1}";
+$l['pmdata_no_recipients'] = "Bạn chưa nhập tên người nhận. Bạn cần nhập ít nhất một tên người nhận ở dòng 'Đến'.";
+$l['pmdata_too_many_recipients'] = "Bạn chỉ được chấp nhận để gởi tin nhắn cho {1} người.";
+$l['pmdata_recipient_is_ignoring'] = "Bạn không thể gởi tin nhắn cho {1} bởi vì bạn nằm trong danh sách đen của họ.";
+$l['pmdata_recipient_has_buddy_only'] = "Bạn không thể gởi tin nhắn cho {1} bởi vì bạn không nằm trong danh sách những người được quyền liên lạc của họ.";
+$l['pmdata_recipient_pms_disabled'] = "{1} không sử dụng hệ thống tin nhăn. Bạn không thể gửi tin nhắn cho họ.";
+$l['pmdata_recipient_reached_quota'] = "{1} lượng tin nhắn của người nhận bị quá tải, vì vậy tin nhắn của bạn không thể gửi được.";
+$l['pmdata_pm_flooding'] = "Bạn đang cố gửi quá nhiều tin nhắn trong một thời gian ngắn, xin hãy chờ trong {1} giây trước khi gửi tiếp.";
+$l['pmdata_pm_flooding_one_second'] = "Bạn đang cố gửi quá nhiều tin nhắn trong một thời gian ngắn, xin hãy chờ trong 1 giây trước khi gửi tiếp.";
+?>
