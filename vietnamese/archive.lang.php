2c99f301c87657ba71d007637949500e8bbca65f
Nguyễn Đình Quân <mrnarga@gmail.com>
2012-03-19 10:30:54 +0700

    archive.lang.php translated, update some comments

---
+++ vietnamese/archive.lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * MyBB 1.6 Vietnamese Language Pack
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ * 
+ * $Id: archive.lang.php Mon 19 Mar 2012 10:28:26 AM ICT narga 
+ */
+
+$l['archive_fullversion'] = "Full Version:";
+$l['archive_replies'] = "Replies";
+$l['archive_reply'] = "Reply";
+$l['archive_pages'] = "Pages:";
+$l['archive_note'] = "You're currently viewing a stripped down version of our content. <a href=\"{1}\">View the full version</a> with proper formatting.";
+$l['archive_reference_urls'] = "Reference URL's";
+$l['archive_nopermission'] = "Sorry, you do not have permission to access this resource.";
+$l['error_nothreads'] = "There are currently no threads in this forum.";
+$l['error_unapproved_thread'] = "This thread is unapproved. Please <a href=\"{1}\">view the full version</a> to view the contents of this thread.";
+$l['archive_not_found'] = "The requested page was not found on this server.";
+?>
