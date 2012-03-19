915bedb248d9a8129606ab3d89ba6df6c3766371
Nguyễn Đình Quân <mrnarga@gmail.com>
2012-03-19 10:48:59 +0700

    Make changelog

--- vietnamese/archive.lang.php
+++ vietnamese/archive.lang.php
@@ -3,17 +3,16 @@
  * MyBB 1.6 English Language Pack
  * Copyright 2010 MyBB Group, All Rights Reserved
  * 
- * $Id: archive.lang.php 5365 2011-02-16 21:59:02Z MattR $
+ * $Id: archive.lang.php 5365 2011-02-16 21:59:02Z  $
  */
-
-$l['archive_fullversion'] = "Full Version:";
-$l['archive_replies'] = "Replies";
-$l['archive_reply'] = "Reply";
-$l['archive_pages'] = "Pages:";
-$l['archive_note'] = "You're currently viewing a stripped down version of our content. <a href=\"{1}\">View the full version</a> with proper formatting.";
-$l['archive_reference_urls'] = "Reference URL's";
-$l['archive_nopermission'] = "Sorry, you do not have permission to access this resource.";
-$l['error_nothreads'] = "There are currently no threads in this forum.";
-$l['error_unapproved_thread'] = "This thread is unapproved. Please <a href=\"{1}\">view the full version</a> to view the contents of this thread.";
-$l['archive_not_found'] = "The requested page was not found on this server.";
+$$l['archive_fullversion'] = "Phiên bản đầy đủ:";
+$l['archive_replies'] = "Trả lời";
+$l['archive_reply'] = "Trả lời";
+$l['archive_pages'] = "Trang:";
+$l['archive_note'] = "Bạn đang xem phiên bản rút gọn.  Bạn có thể xem <a href=\"{1}\">bản đầy đủ</a>.";
+$l['archive_reference_urls'] = "Địa chỉ tham chiếu";
+$l['archive_nopermission'] = "Bạn không được phép truy cập khu vực này.";
+$l['error_nothreads'] = "Hiện tại chưa có chủ đề nào trong diễn đàn này.";
+$l['error_unapproved_thread'] = "Chủ đề này chưa được chấp nhận. Xem <a href=\"{1}\">phiên bản đầy đủ</a> của chủ đề này.";
+$l['archive_not_found'] = "Không tìm thấy tài liệu nào trên máy chủ này."; 
 ?>
